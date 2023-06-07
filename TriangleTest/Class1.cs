18b5348465cd213419a781939a579cfd7046321d
Dhruv Panchal <Dpanchal7053@conestogac.on.ca>
2023-06-07 13:31:29 -0400

    Add NUnit test for valid isosceles triangle

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -26,5 +26,52 @@ namespace TriangleTest
             // Assert
             Assert.AreEqual(expected, result);
         }
+        [Test]
+        public void AnalyzeTriangle_Input10and10and6_OutputValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstAngle = 10;
+            int secondAngle = 10;
+            int thirdAngle = 6;
+            string expected = "An ISOSCELES triangle is formed";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void AnalyzeTriangle_Input10and6and10_OutputValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstAngle = 10;
+            int secondAngle = 6;
+            int thirdAngle = 10;
+            string expected = "An ISOSCELES triangle is formed";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void AnalyzeTriangle_Input6and10and10_OutputValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstAngle = 6;
+            int secondAngle = 10;
+            int thirdAngle = 10;
+            string expected = "An ISOSCELES triangle is formed";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
     }
 }
